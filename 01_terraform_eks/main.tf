e66e93931f717f6981b8d0171ace83af6fce304b
tawanchaiii <tawanchai_champ@hotmail.com>
2023-07-08 14:16:29 +0700

    Change vpc version to 5.0.0

--- 01_terraform_eks/main.tf
+++ 01_terraform_eks/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-1"
 }
 
 data "aws_availability_zones" "available" {}
@@ -37,7 +37,7 @@ resource "local_file" "kubeconfig" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+  version = "5.0.0"
 
   name                 = "k8s-vpc"
   cidr                 = "172.16.0.0/16"
